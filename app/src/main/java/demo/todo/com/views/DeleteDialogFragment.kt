e9b3cf501e976a625ced324a829a28f81d83cb4f
htandale <htandale@tracfone.com>
2019-03-29 14:55:57 -0400

    Updated app icon

--- app/src/main/java/demo/todo/com/views/DeleteDialogFragment.kt
+++ app/src/main/java/demo/todo/com/views/DeleteDialogFragment.kt
@@ -6,26 +6,25 @@ import android.support.v4.app.DialogFragment
 import android.content.DialogInterface
 import android.app.AlertDialog
 import android.content.ContextWrapper
+import android.support.design.button.MaterialButton
+import android.widget.Button
 import demo.todo.com.R
 
 
 class DeleteDialogFragment : DialogFragment() {
+    lateinit var alertDialog : AlertDialog
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return AlertDialog.Builder(ContextWrapper(activity))
-            .setTitle(R.string.alert)
-            .setMessage(R.string.are_you_sure)
-            .setPositiveButton(
-                android.R.string.yes
-            ) { dialog, which ->
-                run {
-                    val toDoViewModel = (activity as MainActivity).toDoViewModel
-                    toDoViewModel.deleteAllTodoItems()
-                }
+        var contentView = activity?.layoutInflater?.inflate(R.layout.delete_dialog, null, false)
+        contentView?.findViewById<Button>(R.id.cancel_button)?.setOnClickListener { alertDialog.dismiss() }
+        contentView?.findViewById<Button>(R.id.delete_button)?.setOnClickListener {
+            run {
+                val toDoViewModel = (activity as MainActivity).toDoViewModel
+                toDoViewModel.deleteAllTodoItems()
             }
-            .setNegativeButton(
-                android.R.string.no
-            ) { dialog, which -> dialog.dismiss() }
+        }
+        alertDialog = AlertDialog.Builder(ContextWrapper(activity))
+            .setView(contentView)
             .create()
+        return alertDialog
     }
-
 }
--- app/src/main/java/demo/todo/com/views/NewToDoFragment.kt
+++ app/src/main/java/demo/todo/com/views/NewToDoFragment.kt
@@ -6,19 +6,16 @@ import android.view.*
 import demo.todo.com.R
 import demo.todo.com.db.entities.ToDoItemEntity
 import demo.todo.com.viewmodels.ToDoViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_new_to_do.*
 
 class NewToDoFragment: Fragment() {
     var title : String?= ""
-    var content : String?= ""
+    private var content : String?= ""
     private var toDoItemEntity : ToDoItemEntity? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_new_to_do,container,false)
     }
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.menu_save,menu)
-    }
-
     private lateinit  var toDoViewModel: ToDoViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
